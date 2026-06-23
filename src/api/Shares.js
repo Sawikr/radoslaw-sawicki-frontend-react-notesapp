513775cd9c18b6fd5a25ad4fcf28ea9aa74f022b
Radoslaw Sawicki <48261599+Sawikr@users.noreply.github.com>
2026-06-23 22:34:28 +0200

    Notes: improving stock market shares api

--- src/api/Shares.js
+++ src/api/Shares.js
@@ -29,13 +29,20 @@ function Shares() {
     const [changeColor1, setChangeColor1] = useState(true);
     const [changeColor2, setChangeColor2] = useState(true);
     const [changeColor3, setChangeColor3] = useState(true);
+    const [changeColorStatus1, setChangeColorStatus1] = useState(true);
+    const [changeColorStatus2, setChangeColorStatus2] = useState(true);
+    const [changeColorStatus3, setChangeColorStatus3] = useState(true);
     const [loading, setLoading] = useState(true);
-    const [loadingOne, setLoadingOne] = useState(false);
     const [showReturnButton, setShowReturnButton] = useState(false);
-    const currentDate = Date.now().valueOf();
+    const [currentDate, setCurrentDate] = useState();
+    //const currentDate = Date.now().valueOf();
     const isAuth = isUserLoggedIn();
     const navigate = useNavigate();
     const wait = (n) => new Promise((resolve) => setTimeout(resolve, n));
+    const list = useState([]);
+    const [closeResponse, setCloseResponse] = useState([]);
+    const [response, setResponse] = useState([]);
+    const [status, getStatus] = useState();
 
     function returnButton() {
         navbarToken(true);
@@ -47,39 +54,53 @@ function Shares() {
         setShowReturnButton(true);
     }
 
-    function getChange(response) {
-        let close = response.data.data[0].close;
-        let openClose = response.data.data[1].close;
-        let change = (close - openClose) / openClose * 100;
+    function getChange(open, close) {
+        const change = (close - open) / open * 100;
         return change;
     }
 
-    function color(change) {
+    function checkColor(change) {
         if (change > 0) {
             return true;
         } else
             return false;
     }
 
-    function getData1() {
+    function checkStatus(status) {
+        if (status === "OK") {
+            return true;
+        } else
+            return false;
+    }
+
+    function getCurrentDate(date) {
+        const currentDate = date;
+            return currentDate;
+    }
+
+    async function getData1() {
         NotesService.getShare1()
             .then(async response => {
                 console.log('Printing response!', response.data);
 
-                const closeResponse = response.data.data[0].close;
-                const openResponse = response.data.data[0].open;
-                const volumeResponse = response.data.data[0].volume;
+                const list = response.data;
+                console.log('Printing list!', list);
+                setShare1(list);
+                const currentDate = share1.from;
+                const closeResponse = share1.close;
+                const openResponse = share1.open;
                 //console.log('Close is ' + closeResponse + '!');
+                //console.log('Open is ' + openResponse + '!');
 
-                setClose1(closeResponse);
+                getCurrentDate(currentDate);
+                setCurrentDate(currentDate);
 
-                let change = getChange(response);
+                const change = getChange(openResponse, closeResponse);
+                const status = share1.status;
                 setChange1(change);
-                setOpen1(openResponse);
-                setVolume1(volumeResponse);
-                setChangeColor1(color(change));
-
-                setLoadingOne(false);
+                setChangeColor1(checkColor(change));
+                setChangeColorStatus1(checkStatus(status));
+                setLoading(false);
                 await showButton();
             })
             .catch(error => {
@@ -89,21 +110,24 @@ function Shares() {
 
     function getData2() {
         NotesService.getShare2()
-            .then(response => {
+            .then(async response => {
                 console.log('Printing response!', response.data);
 
-                const closeResponse = response.data.data[0].close;
-                const openResponse = response.data.data[1].open;
-                const volumeResponse = response.data.data[0].volume;
+                const list = response.data;
+                console.log('Printing list!', list);
+                setShare2(list);
+                const closeResponse = share2.close;
+                const openResponse = share2.open;
                 //console.log('Close is ' + closeResponse + '!');
+                //console.log('Open is ' + openResponse + '!');
 
-                let change = getChange(response);
+                const change = getChange(openResponse, closeResponse);
+                const status = share2.status;
                 setChange2(change);
-                setOpen2(openResponse);
-                setVolume2(volumeResponse);
-                setChangeColor2(color(change));
-
-                setClose2(closeResponse);
+                setChangeColor2(checkColor(change));
+                setChangeColorStatus2(checkStatus(status));
+                setLoading(false);
+                await showButton();
             })
             .catch(error => {
                 console.log('An error occurred!', error);
@@ -112,21 +136,24 @@ function Shares() {
 
     function getData3() {
         NotesService.getShare3()
-            .then(response => {
+            .then(async response => {
                 console.log('Printing response!', response.data);
 
-                const closeResponse = response.data.data[0].close;
-                const openResponse = response.data.data[1].open;
-                const volumeResponse = response.data.data[0].volume;
+                const list = response.data;
+                console.log('Printing list!', list);
+                setShare3(list);
+                const closeResponse = share3.close;
+                const openResponse = share3.open;
                 //console.log('Close is ' + closeResponse + '!');
+                //console.log('Open is ' + openResponse + '!');
 
-                let change = getChange(response);
+                const change = getChange(openResponse, closeResponse);
+                const status = share3.status;
                 setChange3(change);
-                setOpen3(openResponse);
-                setVolume3(volumeResponse);
-                setChangeColor3(color(change));
-
-                setClose3(closeResponse);
+                setChangeColor3(checkColor(change));
+                setChangeColorStatus3(checkStatus(status));
+                setLoading(false);
+                await showButton();
             })
             .catch(error => {
                 console.log('An error occurred!', error);
@@ -142,7 +169,7 @@ function Shares() {
     return (
         <div className="main-content">
             {
-            loadingOne ? (
+            loading ? (
                 <div>
                     {!isAuth &&
                         <div className="loader-container" style={{marginTop: 137}}>
@@ -167,73 +194,109 @@ function Shares() {
                 <Space/>
                 <x-h8>
                     <div className="row mb-2 ml-auto">
-                        NASDAQ:
+                        NASDAQ Stock Market:
                     </div>
                 </x-h8>
                 <div className="row mb-4 ml-auto">
                     <div className="column left">
-                        Close: {JSON.stringify(close1)}
+                        Close: {JSON.stringify(share1.close)}
                         <div>
-                            Open: {JSON.stringify(open1)}
+                            Open: {JSON.stringify(share1.open)}
                         </div>
                         <div>
-                            Volume: {JSON.stringify(volume1)}
+                            High: {JSON.stringify(share1.high)}
+                        </div>
+                        <div>
+                            Low: {JSON.stringify(share1.low)}
+                        </div>
+                        <div>
+                            Status:
+                            {changeColorStatus1 &&
+                                <i className="ml-1" style={{color: "green"}}>{share1.status}</i>
+                            }
+                            {!changeColorStatus1 &&
+                                <i className="ml-1" style={{color: "red"}}>{share1.status}</i>
+                            }
                         </div>
                     </div>
                     <div className="column right">
                         {changeColor1 &&
-                            <i className="ml-1" style={{color: "green"}}>{change1.toFixed(2)}</i>
+                            <i className="ml-5" style={{color: "green"}}>{change1.toFixed(2)}</i>
                         }
                         {!changeColor1 &&
-                            <i className="ml-1" style={{color: "red"}}>{change1.toFixed(2)}</i>
+                            <i className="ml-5" style={{color: "red"}}>{change1.toFixed(2)}</i>
                         } %
                     </div>
                 </div>
                 <x-h8>
                     <div className="row mb-2 ml-auto">
-                        S&P500:
+                        Goldman Physical Gold ETF:
                     </div>
                 </x-h8>
                 <div className="row mb-4 ml-auto">
                     <div className="column left">
-                        Close: {JSON.stringify(close2)}
+                        Close: {JSON.stringify(share2.close)}
                         <div>
-                            Open: {JSON.stringify(open2)}
+                            Open: {JSON.stringify(share2.open)}
                         </div>
                         <div>
-                            Volume: {JSON.stringify(volume2)}
+                            High: {JSON.stringify(share2.high)}
+                        </div>
+                        <div>
+                            Low: {JSON.stringify(share2.low)}
+                        </div>
+                        <div>
+                            Status:
+                            {changeColorStatus2 &&
+                                <i className="ml-1" style={{color: "green"}}>{share2.status}</i>
+                            }
+                            {!changeColorStatus2 &&
+                                <i className="ml-1" style={{color: "red"}}>{share2.status}</i>
+                            }
                         </div>
                     </div>
                     <div className="column right">
                         {changeColor2 &&
-                            <i className="ml-1" style={{color: "green"}}>{change2.toFixed(2)}</i>
+                            <i className="ml-5" style={{color: "green"}}>{change2.toFixed(2)}</i>
                         }
                         {!changeColor2 &&
-                            <i className="ml-1" style={{color: "red"}}>{change2.toFixed(2)}</i>
+                            <i className="ml-5" style={{color: "red"}}>{change2.toFixed(2)}</i>
                         } %
                     </div>
                 </div>
                 <x-h8>
                     <div className="row mb-2 ml-auto">
-                        WIG20:
+                        NVIDIA Corporation:
                     </div>
                 </x-h8>
                 <div className="row mb-4 ml-auto">
                 <div className="column left">
-                    Close: {JSON.stringify(close3)}
+                    Close: {JSON.stringify(share3.close)}
                     <div>
-                        Open: {JSON.stringify(open3)}
+                        Open: {JSON.stringify(share3.open)}
                     </div>
                     <div>
-                        Volume: {JSON.stringify(volume3)}
+                        High: {JSON.stringify(share3.high)}
+                    </div>
+                    <div>
+                        Low: {JSON.stringify(share3.low)}
+                    </div>
+                    <div>
+                        Status:
+                        {changeColorStatus3 &&
+                            <i className="ml-1" style={{color: "green"}}>{share3.status}</i>
+                        }
+                        {!changeColorStatus3 &&
+                            <i className="ml-1" style={{color: "red"}}>{share3.status}</i>
+                        }
                     </div>
                 </div>
                 <div className="column right">
                     {changeColor3 &&
-                        <i className="ml-1" style={{color: "green"}}>{change3.toFixed(2)}</i>
+                        <i className="ml-5" style={{color: "green"}}>{change3.toFixed(2)}</i>
                     }
                     {!changeColor3 &&
-                        <i className="ml-1" style={{color: "red"}}>{change3.toFixed(2)}</i>
+                        <i className="ml-5" style={{color: "red"}}>{change3.toFixed(2)}</i>
                     } %
                 </div>
                 </div>
